92f6152b64d715f0e94f3effe8a7e99b9572b8b6
adrianhelllo <hetessyadrian@gmail.com>
2026-06-29 19:14:02 +0200

    command handler

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const token = process.env.BOT_TOKEN;
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
@@ -7,3 +10,23 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.login(token);
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const folders = fs.readdirSync(foldersPath);
+
+for (const folder of folders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commands = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log('missing property');
+		}
+	}
+}
